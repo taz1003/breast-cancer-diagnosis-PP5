c08aaf85f528e0efd8a9d6916e4f5e318a188482
taz1003 <li9vyrit@students.codeinstitute.net>
2025-08-19 01:33:55 +0200

    Removed ydata-profiling and xgboost due to disturbance in deploying to heroku

--- bin/prepare-heroku.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-set -euo pipefail
-
-echo 'Preparing Heroku deploy'
-
-heroku buildpacks:clear
-heroku buildpacks:add heroku/nodejs
-heroku buildpacks:add https://github.com/heroku/heroku-buildpack-static
